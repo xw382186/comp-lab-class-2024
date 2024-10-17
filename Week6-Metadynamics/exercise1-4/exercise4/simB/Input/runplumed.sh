75dbb31eece35806088a007dabd437cd04b1a024
Wei Xia <wx2237@log-2.hpc.nyu.edu>
2024-10-17 13:28:15 -0400

    Completed all assignments, including 2D metadynamics and reweighting

---
+++ Week6-Metadynamics/exercise1-4/exercise4/simB/Input/runplumed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=gmx_metadynamics        # 作业名
+#SBATCH --output=gmx_output_%j.log         # 标准输出日志文件（%j将被作业ID替换）
+#SBATCH --error=gmx_error_%j.log           # 错误输出日志文件
+#SBATCH --time=8:00:00                    # 运行时间上限 (例如 48小时)
+#SBATCH --nodes=1                          # 需要的节点数
+#SBATCH --ntasks=1                         # 任务数（一般是MPI进程数）
+#SBATCH --cpus-per-task=32                  # 每个任务使用的CPU核心数
+#SBATCH --partition=cs                     # 作业提交到的分区名称
+
+# 加载 GROMACS 和 PLUMED 环境
+source /scratch/work/courses/CHEM-GA-2671-2024fa/software/gromacs-2019.6-plumedSept2020/bin/GMXRC.bash.modules
+
+# 运行GROMACS与PLUMED模拟
+gmx_mpi mdrun -s topolB.tpr -nsteps 5000000 -plumed plumed.dat
